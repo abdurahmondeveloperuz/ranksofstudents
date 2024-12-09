40d65939261b5a0edd06267e4e0d379e2faef37a
abdurahmondeveloperuz <154701401+abdurahmondeveloperuz@users.noreply.github.com>
2024-12-09 16:13:18 +0500

    Delete app directory

--- app/students/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Student, Class
-
-class StudentSerializer(serializers.ModelSerializer):
-    rank = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = Student
-        fields = ['first_name', 'last_name', 'score', 'rank']
--- app/students/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .api import views as api_views
-
-urlpatterns = [
-    path('rankings/', views.rankings, name='rankings'),
-    path('rankings/download/', views.download_rankings_csv, name='download_rankings'),
-    path('api/set_class', api_views.set_class, name='set_class'),
-]
